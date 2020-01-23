4a718b0cf350d4f09a9d80961f59b931a6215460
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-01-23 09:44:26 +0200

    Improve tshark test.

--- tests/transform/tshark_test.py
+++ tests/transform/tshark_test.py
@@ -1,5 +1,5 @@
-from meeshkan.transform.tshark import convert_pcap, transform_tshark
-from hamcrest import assert_that, has_length
+from meeshkan.transform.tshark import convert_pcap
+from hamcrest import assert_that, has_length, has_entry
 
 PCAP_SAMPLE = 'resources/recordings.pcap'
 
@@ -7,3 +7,4 @@ def test_convert_pcap():
     recordings_gen = convert_pcap(PCAP_SAMPLE)
     recordings = list(recordings_gen)
     assert_that(recordings, has_length(168))
+    assert_that(recordings[0], has_entry("req", has_entry("path", "/user/repos")))
