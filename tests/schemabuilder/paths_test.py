ea6fadbbd6616cb413c1f067a4e7854bd9e29123
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-01-27 11:16:41 +0200

    Work on paths code.

--- tests/schemabuilder/paths_test.py
+++ tests/schemabuilder/paths_test.py
@@ -5,6 +5,7 @@ def test_path_to_regex():
     as_regex = path_to_regex('/pets/{id}', id={'type': 'number'})
 
     assert as_regex.pattern == '^\\/pets\\/\\d+$'
-    assert as_regex.match("/pets/32")
-    assert not as_regex.match("/pets/32/")
-    assert not as_regex.match("/pets/foo")
+    assert as_regex.pattern == r"""^\/pets\/\d+$"""
+    assert as_regex.match("/pets/32"), "Expected /pets/32 to match"
+    assert not as_regex.match("/pets/32/"), "Did not expect /pets/32/ to match"
+    assert not as_regex.match("/pets/foo"), "Did not expect /pets/foo to match"
