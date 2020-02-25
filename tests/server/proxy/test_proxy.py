bd7e6540d9e00bb49b29d6dc85477108192dbaeb
Nikolay <aby2sz@gmail.com>
2020-02-25 20:04:37 +0300

    no TCP_NODELAY, Host header fix

--- tests/server/proxy/test_proxy.py
+++ tests/server/proxy/test_proxy.py
@@ -3,13 +3,13 @@ import urllib.parse
 from unittest.mock import Mock
 
 import pytest
-from http_types import Request, Response, HttpMethod, Protocol
-from tornado.testing import bind_unused_port
-
+from http_types import HttpMethod, Protocol
 from meeshkan.server import make_mocking_app
 from meeshkan.server.proxy.proxy import RecordProxy
 from meeshkan.server.utils.data_callback import DataCallback
-from meeshkan.server.utils.routing import PathRouting, StaticRouting
+from meeshkan.server.utils.routing import PathRouting, StaticRouting, HeaderRouting
+from tornado.httpclient import HTTPRequest
+from tornado.testing import bind_unused_port
 
 
 @pytest.fixture
@@ -19,7 +19,7 @@ def app():
 
 
 @pytest.mark.gen_test
-def test_proxy(http_client, base_url):
+def test_path_proxy(http_client, base_url):
     server = None
     data_callback = Mock(spec=DataCallback)
     try:
@@ -28,27 +28,57 @@ def test_proxy(http_client, base_url):
         server.add_socket(sock)
         response = yield http_client.fetch('http://localhost:{}/{}/pets'.format(port, base_url))
 
-        assert response.code == 200
+        assert 200 == response.code
         rb = json.loads(response.body)
-        assert type(rb) == type([])
+        assert type([]) == type(rb)
     finally:
         if server is not None:
             server.stop()
 
     yield http_client.close()
 
-    class RequestMatcher:
-        def __eq__(self, other: Request):
-            return other.method == HttpMethod('get') and \
-                   other.pathname == '/pets' and \
-                   other.path == '/pets' and \
-                   other.query == {}
+    assert len(data_callback.log.call_args_list) == 1
 
-    class ResponseMatcher:
-        def __eq__(self, other: Response):
-            return
+    base_url_sp = urllib.parse.urlsplit(base_url)
+    host = '{}:{}'.format(base_url_sp.hostname, base_url_sp.port)
 
-    # data_callback.log.assert_called_with(RequestMatcher(), ResponseMatcher())
+    request = data_callback.log.call_args_list[0][0][0]
+    assert HttpMethod('get') == request.method
+    assert Protocol('http') == request.protocol
+    assert '/pets' == request.pathname
+    assert '/pets' == request.path
+    assert {} == request.query
+    assert host == request.host
+    assert host == request.headers['Host']
+
+    response = data_callback.log.call_args_list[0][0][1]
+    assert 200 == response.statusCode
+    assert isinstance(response.bodyAsJson, list)
+    assert isinstance(response.body, str)
+    assert len(response.body) > 0
+
+
+@pytest.mark.gen_test
+def test_header_proxy(http_client, base_url):
+    server = None
+    data_callback = Mock(spec=DataCallback)
+    try:
+        sock, port = bind_unused_port()
+        server = RecordProxy(data_callback, HeaderRouting())
+        server.add_socket(sock)
+        host_url = urllib.parse.urlsplit(base_url)
+        response = yield http_client.fetch(HTTPRequest('http://localhost:{}/pets'.format(port),
+                                                       headers={'host': '{}:{}'.format(host_url.hostname,
+                                                                                       host_url.port)}))
+
+        assert 200 == response.code
+        rb = json.loads(response.body)
+        assert type([]) == type(rb)
+    finally:
+        if server is not None:
+            server.stop()
+
+    yield http_client.close()
 
     assert len(data_callback.log.call_args_list) == 1
 
