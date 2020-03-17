876b621ad61eaa634e1316265b6d900207b94ae8
nakulbansal1609 <nakul.bansalsat@gmail.com>
2020-03-18 01:05:17 +0200

    more changes

---
+++ meeshkan/build/nlp/schema_normalizer/schema_normalizer/schema_paths/schema_compare.py
@@ -0,0 +1,21 @@
+import json
+import json
+import sys
+
+
+def exact_match(list1, list2):
+    if list1 == list2:
+        return True
+    else:
+        return False
+
+def compare_nested_schema(list1, list2):
+    match_list = list()
+    for each1 in list1:
+        for key1, value1 in each1.items():
+            for each2 in list2:
+                for key2, value2 in each2.items():
+                    if exact_match(value1, value2):
+                        match_list.append((key1, key2))
+                        break
+    return match_list[:1]
---
+++ meeshkan/build/nlp/schema_normalizer/schema_normalizer/schema_paths/schema_reference.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append('..')
+from schema_paths.parse_openapi_schema import parse_schema
+from schema_paths.schema_compare import compare_nested_schema
+from schema_paths.schema_compare import compare_nested_schema
+
+
+def check_and_create_ref(specs, path_tuple):
+    all_paths_dict = {key : [] for key in path_tuple}
+    nested_paths_dict = all_paths_dict.copy()
+    methods = ['get', 'post']
+    
+    for path in path_tuple:
+        for method in specs['paths'][path].keys():
+            if method in methods:
+                schema = specs['paths'][path][method]['responses']['200']['content']['application/json']['schema']
+                all_paths_dict[path].append({method : schema})
+                break  # To ensure that only single method is picked for any endpoint
+    
+    # Now lets us get the nested structure for schema
+    for keys, values in all_paths_dict.items():
+        for method in values[0].keys():
+            nested_paths_dict[keys] = parse_schema(values[0][method])
+    
+    # Now we need to compare the two schemas for best refernce
+    best_tuple = compare_nested_schema(nested_paths_dict[path_tuple[0]], nested_paths_dict[path_tuple[1]])
+                                    
+    print(best_tuple)
+    
+    
+
+
+
+def create_ref_path(tuple1):
+    schema_name = None
+    schema_comp = '#/components/schema/'
+    for item in tuple1:
+        if item != '$schema':
+            item = item.split('#')
+            for levels in item:
+                schema_name = levels.split('@')[0]
+    
+
+    if schema_name is None:
+        return ('schema1' ,schema_comp + 'schema1')
+    else:
+        return (schema_name, schema_comp + schema_name)
