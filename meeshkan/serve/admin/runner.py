f96147e8becef8e670628657425e2dc49dcb8e71
Fredrik Fornwall <fredrik@fornwall.net>
2020-03-17 14:25:31 +0100

    Avoid ugly emoji for admin endpoint

--- meeshkan/serve/admin/runner.py
+++ meeshkan/serve/admin/runner.py
@@ -40,4 +40,4 @@ def start_admin(port):
     app = make_admin_app()
     http_server = HTTPServer(app)
     http_server.listen(port)
-    logger.info("⚙ Admin   http://localhost:%s/admin", port)
+    logger.info("• Admin   http://localhost:%s/admin", port)
