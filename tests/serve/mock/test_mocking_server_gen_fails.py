d454b7a7866775a1465d0003393d74a65f9c910a
Mike Solomon <mike@meeshkan.com>
2020-03-18 09:57:28 +0200

    New test

--- tests/serve/mock/test_mocking_server_gen_fails.py
+++ tests/serve/mock/test_mocking_server_gen_fails.py
@@ -41,3 +41,13 @@ def test_mocking_server_pets(http_client, base_url, test_sink):
         assert False  # what's a good way to do this?
     except HTTPClientError as e:
         assert str(e) == "HTTP 400: Bad Request"
+
+
+@pytest.mark.gen_test
+def test_mocking_server_pets(http_client, base_url, test_sink):
+    req = HTTPRequest(base_url + "/fewewsdfeaf", headers={"Host": "bad.api.io"})
+    try:
+        yield http_client.fetch(req)
+        assert False  # what's a good way to do this?
+    except HTTPClientError as e:
+        assert str(e) == "HTTP 501: Not Implemented"
