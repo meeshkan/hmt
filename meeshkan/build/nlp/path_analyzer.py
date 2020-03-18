f1fdc89e266d85f1d753df95bfafef17b4130e65
Nikolay <aby2sz@gmail.com>
2020-03-18 13:33:26 +0300

    path analyzer refactoring

--- meeshkan/build/nlp/path_analyzer.py
+++ meeshkan/build/nlp/path_analyzer.py
@@ -1,16 +1,21 @@
 from enum import Enum
+from typing import Optional
+
+from dataclasses import dataclass
 
 
-class PathItemTypes(Enum):
-    ENTITY=0
-    ACTION=1
-    ID=2
-    GROUP_ID=3#page numbers or something
+@dataclass(frozen=True)
+class PathItems:
+    entity: Optional[str]
+    action: Optional[str]
+    id: Optional[str]
+    group_id: Optional[str]
+
 
 class PathAnalyzer:
     def __init__(self):
         pass
 
     def extract_values(self, path): #path="/payemnts/35234"
-        return {PathItemTypes.ENTITY: "payment", PathItemTypes.ACTION: "confirm",
-                PathItemTypes.ID: None, PathItemTypes.GROUP_ID: None}
+        return PathItems(entity="payment", action="confirm",
+                id=None, group_id=None)
