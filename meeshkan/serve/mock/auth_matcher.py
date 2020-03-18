9f88b4714ece1dea1dab65e4d4a7a805969524d2
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-03-18 17:01:08 +0200

    Start working on auth matching.

---
+++ meeshkan/serve/mock/auth_matcher.py
@@ -0,0 +1,49 @@
+from openapi_typed_2.openapi import (
+    AuthorizationCodeOAuthFlow,
+    OAuth2SecurityScheme,
+    SecurityScheme,
+)
+from http_types import Request
+from typing import Union, Optional
+
+from six import i
+from .specs import OpenAPISpecification
+from .matcher import truncate_path
+
+
+def matches_to_oauth2(
+    truncated_path: str, scheme: OAuth2SecurityScheme
+) -> Optional[Union[AuthorizationCodeOAuthFlow]]:
+    flows = scheme.flows
+
+    if flows.authorizationCode is not None:
+        auth_flow = flows.authorizationCode
+        if (
+            auth_flow.authorizationUrl == truncated_path
+            or auth_flow.tokenUrl == truncated_path
+            or auth_flow.refreshUrl == truncated_path
+        ):
+            return auth_flow
+
+    # TODO Other flows
+    return None
+
+
+def match_request_to_security_scheme(
+    req: Request, spec: OpenAPISpecification
+) -> Optional[SecurityScheme]:
+    components = spec.api.components
+
+    if components is None or components.securitySchemes is None:
+        return None
+
+    security_schemes = components.securitySchemes
+
+    truncated_path = truncate_path(req.pathname, spec.api, req)
+
+    for _, scheme in security_schemes.items():
+        if isinstance(scheme, OAuth2SecurityScheme):
+            if matches_to_oauth2(truncated_path, scheme):
+                return scheme
+
+    return None
---
+++ tests/serve/mock/test_auth_matcher.py
@@ -0,0 +1,25 @@
+from openapi_typed_2.openapi import OAuth2SecurityScheme
+from meeshkan.serve.mock.auth_matcher import (
+    match_request_to_security_scheme,
+    matches_to_oauth2,
+)
+
+from meeshkan.serve.mock.matcher import truncate_path
+from meeshkan.serve.mock.specs import load_specs
+from http_types import Request, RequestBuilder
+
+spec = load_specs("tests/serve/mock/schemas/nordea")[0]
+
+
+req = RequestBuilder.from_url("https://api.nordeaopenbanking.com/personal/v4/authorize")
+
+
+def test_truncate_path():
+    truncated = truncate_path(req.pathname, spec.api, req)
+    assert truncated == "/v4/authorize"
+
+
+def test_match_to_oauth():
+    match = match_request_to_security_scheme(req, spec)
+    assert match is not None
+    assert isinstance(match, OAuth2SecurityScheme)
