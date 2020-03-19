a04b9376671b8546f29b128fa6ea5e74ba099e88
MariaMalitckaya <maria.malitckaya@gmail.com>
2020-03-19 16:36:52 +0200

    take into account id position

--- meeshkan/build/nlp/gib_detect.py
+++ meeshkan/build/nlp/gib_detect.py
@@ -2,25 +2,27 @@
 
 import pickle
 import gibberish_detector.gib_detect_train as gib_detect_train
-from entity_extractor import split_path
-model_data = pickle.load(open('gibberish_detector/gib_model.pki', 'rb'))
+#from entity_extractor import split_path
 
 
-#input=path=>>>
-path='/v3/profiles/saf45gdrg4gsdf/transfers/sdfsr456ygh56ujhgf/payments'
-#path_list=split_path(path)
+#model_data = pickle.load(open('gibberish_detector/gib_model.pki', 'rb'))
 
-def id_classifier(path):
-    id=[]
-    path_list=split_path(path)
-    for item in path_list:
-        l =item
-        model_mat = model_data['mat']
-        threshold = model_data['thresh']
-        if not (gib_detect_train.avg_transition_prob(l, model_mat) > threshold):
-               id.append(item)
-    if len(id)!=0:
-         return id[-1]
-    else:
-         return None
-#print(id_classifier(path))
+
+class IdClassifier:
+
+    def __init__(self):
+         #path='/v3/profiles/saf45gdrg4gsdf/transfers/sdfsr456ygh56ujhgf/payments'
+         self.model_data = pickle.load(open('gibberish_detector/gib_model.pki', 'rb'))
+
+    def id_classifier(self, path_list):
+         id=[]
+         for item in path_list:
+             l =item
+             model_mat = self.model_data['mat']
+             threshold = self.model_data['thresh']
+             if not (gib_detect_train.avg_transition_prob(l, model_mat) > threshold):
+                  id.append(item)
+                  if len(id)!=0:
+                       return id[-1]
+             else:
+                  return None
--- meeshkan/build/nlp/path_analyzer.py
+++ meeshkan/build/nlp/path_analyzer.py
@@ -1,9 +1,10 @@
 from enum import Enum
 from typing import Optional
+import re
 
 from dataclasses import dataclass
 from entity_extractor import EntityExtractor
-from gib_detect import id_classifier
+from gib_detect import IdClassifier
 
 @dataclass(frozen=True)
 class PathItems:
@@ -16,6 +17,27 @@ class PathItems:
 class PathAnalyzer:
     def __init__(self):
         self._entity_extractor = EntityExtractor()
+        self._id_classifier = IdClassifier()
 
-    def extract_values(self, path): #path="/payemnts/35234"
-        return PathItems(entity=self._entity_extractor.get_entity_from_url(path), id=id_classifier(path), action=None, group_id=None)
+    def extract_values(self, path):
+        path_list=path.split('/')[1:]
+        nopunc_string=[]
+        for i in path_list:
+            if len(re.findall('[0-9]+', i))!=0 :
+                 nopunc_string.append(i)
+            else:
+                 i = re.sub('[^0-9a-z]+', ' ', i.lower())
+                 for word in i.split(" "):
+                     nopunc_string.append(word)
+        pos= {value:index for index,value in enumerate(nopunc_string)}
+        maybe_entity = self._entity_extractor._split_pathes(path_list)[-1]
+        maybe_id = self._id_classifier.id_classifier(path_list)
+       # print(type(maybe_id))
+        if maybe_id is not None:
+            if pos[maybe_id]==pos[maybe_entity]+1:
+                    return PathItems(entity=self._entity_extractor.get_entity_from_url(path_list), id=maybe_id, action=None, group_id=None)
+            else:
+                    return PathItems(entity=self._entity_extractor.get_entity_from_url(path_list), id=None, action=None, group_id=None)
+        else:
+             return PathItems(entity=self._entity_extractor.get_entity_from_url(path_list), id=None, action=None, group_id=None)
+ #       return PathItems(entity=self._entity_extractor.get_entity_from_url(path_list), id=id_classifier(path_list), action=None, group_id=None)
