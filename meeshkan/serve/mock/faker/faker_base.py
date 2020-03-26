21455d5528f40232b3fcf304caf065ea5920d9e5
Nikolay <aby2sz@gmail.com>
2020-03-26 12:21:42 +0300

    abc for faker base

--- meeshkan/serve/mock/faker/faker_base.py
+++ meeshkan/serve/mock/faker/faker_base.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Any
 
 from faker import Faker
@@ -8,15 +9,17 @@ from openapi_typed_2 import Operation
 
 
 
-class MeeshkanFakerBase:
+class MeeshkanFakerBase(ABC):
     def __init__(self, text_faker: Faker, request: Request, spec: OpenAPIObject, storage: Storage):
         self._fkr = text_faker
         self._request = request
         self._spec = spec
         self._storage = storage
 
+    @abstractmethod
     def execute(self) -> Any:
-        raise NotImplementedError
+        pass
 
+    @abstractmethod
     def _update_data(self, method: Operation):
         pass
