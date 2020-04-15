6236232d2aba5e5ef9f17d40b7639e73cef108fb
Nikolay <aby2sz@gmail.com>
2020-04-15 17:30:16 +0300

    sort

--- meeshkan/serve/mock/specs.py
+++ meeshkan/serve/mock/specs.py
@@ -1,10 +1,10 @@
 import json
 import os
+import yaml
 from dataclasses import dataclass
 from typing import Sequence, Union
 
 import requests
-import yaml
 from openapi_typed_2 import OpenAPIObject, convert_to_openapi
 from requests.exceptions import RequestException
 
